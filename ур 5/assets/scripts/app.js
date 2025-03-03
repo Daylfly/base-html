0715ea0b553f12a677e950715bee09740676d9fb
Daylfly <dazzlenolik@gmail.com>
2025-03-03 16:37:06 +0300

    upp55

--- ур 5/assets/scripts/app.js
+++ ур 5/assets/scripts/app.js
@@ -1,8 +1,8 @@
-import initTestimonialSlider from "./modules/testimonial-slider.js";
+
 import {initNavigation} from "./modules/navigation.js";
 
 
 document.addEventListener('DOMContentLoaded', () => {
     initNavigation()
-    initTestimonialSlider();
+
 })
